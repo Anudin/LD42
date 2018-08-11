0d48be0c896f547c8c47a1f87078d5405e4f09c1
Anudin <bastian.boehm-vhm@web.de>
2018-08-11 09:19:25 +0200

    Setup base project, initial player movement
    * Basic project structure
    * Basic player movement

--- game/Main.gd
+++ game/Main.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+onready var player = get_node("Player")
+
 func _ready():
 	DebugInfo.visible = true
+	
+	var width = ProjectSettings.get_setting("display/window/size/width")
+	var height = ProjectSettings.get_setting("display/window/size/height")
+	player.position = Vector2(width / 2, height / 2)
---
+++ game/Player/Player.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var max_speed = 180
+var max_acceleration = 120
+
+var velocity = Vector2(0,0)
+var acceleration = Vector2(0,0)
+
+func _input(event):
+	if event.is_action_pressed("player_up"):
+		acceleration.y = -max_acceleration
+	elif event.is_action_pressed("player_down"):
+		acceleration.y = max_acceleration
+	elif event.is_action_pressed("player_left"):
+		acceleration.x = -max_acceleration
+	elif event.is_action_pressed("player_right"):
+		acceleration.x = max_acceleration
+
+func _physics_process(delta):
+	velocity += acceleration * delta
+	velocity = velocity.clamped(max_speed)
+	
+	position += velocity * delta
