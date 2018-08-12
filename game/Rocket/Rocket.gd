57d0620ce4aa6ae7be80bdfaf46202a40c134641
Anudin <bastian.boehm-vhm@web.de>
2018-08-12 20:06:03 +0200

    Add rocket explosions

--- game/Rocket/Rocket.gd
+++ game/Rocket/Rocket.gd
@@ -5,6 +5,9 @@ signal rocket_killed
 onready var player = get_node("/root/Main/Player")
 onready var target_position = player.position
 
+onready var collision_shape = get_node("CollisionShape2D")
+onready var animation_player = get_node("AnimationPlayer")
+
 var explosion_radius
 var initial_velocity = 30
 
@@ -42,7 +45,7 @@ func _physics_process(delta):
 	position += velocity * delta
 	rotate(spin * delta)
 	
-	if not pushed:
+	if not animation_player.is_playing() and not pushed:
 		# TODO Fix this ugly hack, maybe project on a normal
 		if position.distance_to(target_position) < 10:
 			if position.distance_to(player.position) < explosion_radius:
@@ -53,7 +56,7 @@ func _physics_process(delta):
 	update()
 
 func _draw():
-	if not pushed:
+	if not animation_player.is_playing() and not pushed:
 		# Preview explosion radius
 		draw_circle(to_local(target_position), explosion_radius, Color(1, 0, 0, 0.1))
 		draw_circle(to_local(target_position), 4, Color(1, 0, 0, 0.8))
@@ -80,10 +83,18 @@ func _on_Rocket_area_shape_entered(area_id, area, area_shape, self_shape):
 		get_tree().queue_delete(self)
 	elif area.is_in_group("rockets"):
 		emit_signal("rocket_killed")
-		get_tree().queue_delete(self)
+		explode()
 	elif area == player:
 		player.hit_by_rocket()
 		get_tree().queue_delete(self)
 
 func _on_VisibilityNotifier2D_screen_exited():
+	get_tree().queue_delete(self)
+
+func explode():
+	collision_shape.disabled = true
+	velocity *= .3
+	animation_player.play("explode")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().queue_delete(self)
