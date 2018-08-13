a891322bf8c168b54538980bd464c6495a674bd8
Anudin <bastian.boehm-vhm@web.de>
2018-08-13 09:47:29 +0200

    Add wave progression system

--- game/Main.gd
+++ game/Main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# TODO FIX MULTIPLE ROCKETS SPAWNING ON SAME SPAWN
+
 var width = ProjectSettings.get_setting("display/window/size/width")
 var height = ProjectSettings.get_setting("display/window/size/height")
 var center = Vector2(width / 2, height / 2)
@@ -9,13 +11,13 @@ var tscn_rocket = preload("res://Rocket/Rocket.tscn")
 onready var player = get_node("Player")
 onready var label_score = get_node("HUD/Score")
 
-export var shrinking_rate = 20
-export var rocket_rate = 1
+export var shrinking_rate = 5
+export var rocket_rate = .25
 export var explosion_radius = 32
 
 var playarea_radius
 var playarea_initial_radius
-var playarea_min_radius = 100
+var playarea_min_radius
 
 var timer_rocket_spawn
 var tween_expand
@@ -24,9 +26,19 @@ var expanding = false
 const SCORE_BONUS_KILL = 25
 
 var score = 0
+var wave = 0
+var rocket_count
+var time_bonus_factor = 1
+
+func load_level_data():
+	playarea_min_radius = level_data[wave]["min_radius"]
+	rocket_count = level_data[wave]["rocket_count"]
+	explosion_radius = level_data[wave]["rocket_radius"]
+	
+	print(playarea_min_radius)
 
 func _on_rocket_killed():
-	score += (SCORE_BONUS_KILL / 2)
+	score += SCORE_BONUS_KILL
 
 func _ready():
 	randomize()
@@ -45,6 +57,8 @@ func _ready():
 	tween_expand = Tween.new()
 	add_child(tween_expand)
 	tween_expand.connect("tween_completed", self, "expand_completed")
+	
+	load_level_data()
 
 func expand_completed(object, key):
 	expanding = false
@@ -55,37 +69,63 @@ func _physics_process(delta):
 		player.velocity = (center - player.position).normalized() * player.velocity.length()
 
 func timeout_timer_rocket_spawn():
-	if get_tree().get_nodes_in_group("rockets").size() < 4:
+	if get_tree().get_nodes_in_group("rockets").size() < rocket_count:
 		var firebases = get_node("Firebases").get_children()
 		add_rocket(firebases[randi() % firebases.size()].position)
 
 func _process(delta):
-	score += delta
+	score += delta * time_bonus_factor
 	label_score.text = str(int(score))
 	
-	if not expanding and  playarea_radius < playarea_min_radius:
+	if not expanding and playarea_radius < playarea_min_radius:
 		expanding = true
+		
+		if wave < level_data.size() - 1:
+			wave += 1
+			time_bonus_factor += 1
+			load_level_data()
 		
 		tween_expand.remove_all()
 		tween_expand.interpolate_property(self,
 			NodePath("playarea_radius"),
 			playarea_radius,
 			playarea_initial_radius, 
-			1, Tween.TRANS_EXPO, Tween.EASE_IN, 0)
+			.5, Tween.TRANS_EXPO, Tween.EASE_IN, 0)
 		tween_expand.start()
 	elif not expanding:
 		playarea_radius -= delta * shrinking_rate
 	
 	update()
-	
-	if playarea_radius < playarea_min_radius:
-		pass # TODO Next level?
 
 func add_rocket(position):
 	var rocket = tscn_rocket.instance()
-	rocket.init(32)
+	rocket.init(explosion_radius)
 	rocket.position = position
 	add_child(rocket)
 
 func _draw():
 	draw_circle(Vector2(width / 2, height / 2), playarea_radius, Color("333333"))
+
+var level_data = [
+	{
+		"min_radius": 300,
+		"rocket_rate": 1,
+		"rocket_count": 4,
+		"rocket_speed": 30,
+		"rocket_radius": 16
+	},
+	{
+		"min_radius": 200,
+		"rocket_rate": 1,
+		"rocket_count": 4,
+		"rocket_speed": 30,
+		"rocket_radius": 32
+	},
+	{
+		"min_radius": 100,
+		"rocket_rate": 1,
+		"rocket_count": 4,
+		"rocket_speed": 30,
+		"rocket_radius": 48
+	}
+]
