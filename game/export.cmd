d946196e474750db7743484fc66307a94000ebb2
Anudin <bastian.boehm-vhm@web.de>
2018-08-14 17:37:18 +0200

    Add simple build script

---
+++ game/export.cmd
@@ -0,0 +1,3 @@
+D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Windows Desktop" build\Battle_billiard_galactic_edition.pck
+D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Linux/X11" build\Battle_billiard_galactic_edition
+copy /Y build\*.pck build\web\
