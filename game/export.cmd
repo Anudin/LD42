da2f96a00cba15c87104b057464e1058b8d99522
Anudin <bastian.boehm-vhm@web.de>
2018-08-14 22:33:01 +0200

    Adjust build script

--- game/export.cmd
+++ game/export.cmd
@@ -1,8 +1,9 @@
 cd build
-del *.pck web\*.pck Battle_billiard_galactic_edition *.zip 2>nul
+del *.pck web\*.pck *.x86_64 *.zip 2>nul
 
 cd ..
 D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Windows Desktop" build\Battle_billiard_galactic_edition.pck
+D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Linux/X11" build\Battle_billiard_galactic_edition.x86_64
 
 cd build
 copy /Y *.pck web\
@@ -11,8 +12,6 @@ mkdir Battle_billiard_galactic_edition\
 copy /Y *.exe Battle_billiard_galactic_edition\
 copy /Y *.pck Battle_billiard_galactic_edition\
 start /wait 7z a -tzip Battle_billiard_galactic_edition.zip Battle_billiard_galactic_edition\*.exe Battle_billiard_galactic_edition\*.pck
-timeout 2
 rmdir /s /q Battle_billiard_galactic_edition\
 
-cd ..
-D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Linux/X11" build\Battle_billiard_galactic_edition
+del *.pck 2>nul
