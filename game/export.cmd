db8ddaae424ac7c0614ef3d4580fc729aa9fb51b
Anudin <bastian.boehm-vhm@web.de>
2018-08-14 18:01:30 +0200

    Update build script

--- game/export.cmd
+++ game/export.cmd
@@ -1,3 +1,5 @@
+del build\*.pck build\web\*.pck build\Battle_billiard_galactic_edition 2>nul
 D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Windows Desktop" build\Battle_billiard_galactic_edition.pck
 D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Linux/X11" build\Battle_billiard_galactic_edition
 copy /Y build\*.pck build\web\
+7z a -tzip build\Battle_billiard_galactic_edition.zip build\*.exe build\*.pck
