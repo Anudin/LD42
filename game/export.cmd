510f30c55df5b4fc98edb000d982e5c62a6d912d
Anudin <bastian.boehm-vhm@web.de>
2018-08-15 09:36:10 +0200

    Refactor build script

--- game/export.cmd
+++ game/export.cmd
@@ -1,17 +1,30 @@
+REM Requires a Godot.exe and 7z.exe in PATH
+
+REM Requres the .exe file of a Windows build [currently 64-bit only] two folders above your project as in:
+REM --- DIRECTORY ---
+REM Runner.exe
+REM Project\game\project.godot
+
+SET game_name=Battle_billiard_galactic_edition
+
+REM Remove leftovers from previous builds
 cd build
 del *.pck web\*.pck *.x86_64 *.zip 2>nul
 
+REM Generate Windows & Web .pck, generate Linux runnable
 cd ..
-D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Windows Desktop" build\Battle_billiard_galactic_edition.pck
-D:\Godot\Godot_v3.0.6-stable_win64.exe --export "Linux/X11" build\Battle_billiard_galactic_edition.x86_64
+Godot --export "Windows Desktop" build\%game_name%.pck
+Godot --export "Linux/X11" build\%game_name%.x86_64
 
 cd build
+REM Windows & Web use the same format, copy Windows build to web folder
 copy /Y *.pck web\
 
-mkdir Battle_billiard_galactic_edition\
-copy /Y *.exe Battle_billiard_galactic_edition\
-copy /Y *.pck Battle_billiard_galactic_edition\
-start /wait 7z a -tzip Battle_billiard_galactic_edition.zip Battle_billiard_galactic_edition\*.exe Battle_billiard_galactic_edition\*.pck
-rmdir /s /q Battle_billiard_galactic_edition\
+REM Create a temporary folder containing the .exe and .pck files for Windows, zip it and remove the folder
+mkdir %game_name%
+copy /Y ..\..\..\Runner.exe %game_name%\%game_name%.exe
+copy /Y *.pck %game_name%\
+start /wait 7z a -tzip %game_name%.zip %game_name%\*.exe %game_name%\*.pck
+rmdir /s /q %game_name%\
 
 del *.pck 2>nul
