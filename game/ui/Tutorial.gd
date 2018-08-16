a7631bc014d374c2cbeb4c7e1f35b9e5b132bd4e
Anudin <bastian.boehm-vhm@web.de>
2018-08-16 10:37:35 +0200

    Refactor tutorial

--- game/ui/Tutorial.gd
+++ game/ui/Tutorial.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+export(PackedScene) var tscn_next
+
 func _input(event):
 	if event.is_pressed():
 		get_tree().set_input_as_handled()
-		get_tree().change_scene("res://Main.tscn")
+		get_tree().change_scene_to(tscn_next)
 
 func _ready():
+	# Emit particles offscreen to precompile shaders
 	$RocketExplosion.emitting = true
